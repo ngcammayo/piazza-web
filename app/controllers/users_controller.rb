aadb5ea12695b206392403d304e0ce67a536061a
Bert Cammayo <ngcammayo@trends.com.ph>
2023-06-14 21:33:33 +0800

    Build the authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_authentication only: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -7,7 +9,12 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @organization = Organization.create(members: [@user])
-      # TODO: Log in user...
+
+      @app_session = @user.app_sessions.create
+
+      # Log in user
+      log_in(@app_session)
+
       redirect_to root_path, status: :see_other, flash: { success: t(".welcome", name: @user.name) }
     else
       render :new, status: :unprocessable_entity
